3f6991354770b643b3702943e07fcbc532bc14c3
Badran <badran@Badrans-MacBook-Air.local>
2022-06-04 09:33:01 +0200

    DAY003

--- DAY003/DAY003.playground/Contents.swift
+++ DAY003/DAY003.playground/Contents.swift
@@ -1,4 +1,111 @@
 """
+4. How to create and use enums
+"""
+
+var selected = "Monday"
+
+selected = "Tuesday"
+selected = "January"
+selected = "Friday "
+
+//enum Weekday {
+//    case monday
+//    case tuesday
+//    case wednesday
+//    case thursday
+//    case friday
+//}
+
+//var day = Weekday.monday
+//day = Weekday.tuesday
+//day = Weekday.friday
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = .tuesday
+day = .friday
+
+"""
+3. How to use sets for fast data lookup
+"""
+
+//let people = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+
+//print(people)
+
+var people = Set<String>()
+people.insert("Denzel Washington")
+people.insert("Tom Cruise")
+people.insert("Nicolas Cage")
+people.insert("Samuel L Jackson")
+
+people.sorted()
+
+"""
+2. How to store and find data in dictionaries
+"""
+
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+
+print("Name: \(employee[0])")
+print("Job title: \(employee[1])")
+print("Location: \(employee[2])")
+
+
+//print("Name: \(employee[0])")
+//employee.remove(at: 1)
+//print("Job title: \(employee[1])")
+//print("Location: \(employee[2])")
+
+//let employee2 = ["name": "Taylor Swift", "job": "Singer", "location": "Nashville"]
+
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+//print(employee2["name"])
+//print(employee2["job"])
+//print(employee2["location"])
+
+//print(employee2["password"])
+//print(employee2["status"])
+//print(employee2["manager"])
+
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false,
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+
+print(olympics[2012, default: "Unknown"])
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+
+archEnemies["Batman"] = "Penguin"
+
+"""
 1. How to store ordered data in arrays
 """
 
@@ -63,3 +170,4 @@ print(cities.sorted())
 let presidents = ["Bush", "Obama", "Trump", "Biden"]
 let reversedPresidents = presidents.reversed()
 print(reversedPresidents)
+
