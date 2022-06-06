fc16027df34fd1954220d2e724d9f3d2122407ac
Badran <bdrnglm@gmail.com>
2022-06-06 13:41:00 +0200

    How to skip loop items with break and continue

--- DAY006/DAY006.playground/Contents.swift
+++ DAY006/DAY006.playground/Contents.swift
@@ -1,4 +1,34 @@
 """
+3. How to skip loop items with break and continue
+"""
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+
+    print("Found picture: \(filename)")
+}
+
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+"""
 2. How to use a while loop to repeat work
 """
 
