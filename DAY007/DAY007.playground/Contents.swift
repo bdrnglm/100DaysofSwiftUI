679749abbaeee85a72a0b9002182b19c75ce072f
Badran <bdrnglm@gmail.com>
2022-06-07 16:59:05 +0200

    DAY007: How to return multiple values from functions

--- DAY007/DAY007.playground/Contents.swift
+++ DAY007/DAY007.playground/Contents.swift
@@ -1,4 +1,63 @@
 """
+3. How to return multiple values from functions
+"""
+
+func isUppercase(string: String) -> Bool {
+    string == string.uppercased()
+}
+
+//func getUser() -> [String] {
+//    ["Taylor", "Swift"]
+//}
+//
+//let user: [String] = getUser()
+//print("Name: \(user[0]) \(user[1])")
+
+//func getUser() -> [String: String] {
+//    [
+//        "firstName": "Taylor",
+//        "lastName": "Swift"
+//    ]
+//}
+//
+//let user: [String: String] = getUser()
+//print("Name: \(user["firstName", default: "Anonymous"]) \(user["lastName", default: "Anonymous"])")
+
+//func getUser() -> (firstName: String, lastName: String) {
+//    (firstName: "Taylor", lastName: "Swift")
+//}
+//
+//let user: (firstName: String, lastName: String) = getUser()
+//print("Name: \(user.firstName) \(user.lastName)")
+
+//func getUser() -> (firstName: String, lastName: String) {
+//    ("Taylor", "Swift")
+//}
+
+//func getUser() -> (String, String) {
+//    ("Taylor", "Swift")
+//}
+//
+//let user: (String, String) = getUser()
+//print("Name: \(user.0) \(user.1)")
+
+func getUser() -> (firstName: String, lastName: String) {
+    (firstName: "Taylor", lastName: "Swift")
+}
+
+//let user (String, String) = getUser()
+//let firstName = user.firstName
+//let lastName = user.lastName
+//
+//print("Name: \(firstName) \(lastName)")
+//
+//let (firstName, lastName) = getUser()
+//print("Name: \(firstName) \(lastName)")
+
+let (firstName, _) = getUser()
+print("Name: \(firstName)")
+
+"""
 2. How to return values from functions
 """
 
