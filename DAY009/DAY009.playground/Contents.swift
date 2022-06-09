d26a4e38a76ca3b6aedfe9e77154a27ee3373628
Badran <bdrnglm@gmail.com>
2022-06-09 23:59:45 +0200

    DAY009: Checkpoint 5

--- DAY009/DAY009.playground/Contents.swift
+++ DAY009/DAY009.playground/Contents.swift
@@ -1,4 +1,27 @@
 """
+Checkpoint 5
+
+Your job is to:
+
+1. Filter out any numbers that are even
+2. Sort the array in ascending order
+3. Map them to strings in the format “7 is a lucky number”
+4. Print the resulting array, one item per line
+"""
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+let oddLuckyNumbers = luckyNumbers.filter { $0 % 2 != 0 }
+
+let ascendingOddLuckyNumber = oddLuckyNumbers.sorted { $0 < $1 }
+
+let formatedAscendingOddLuckyNumber = ascendingOddLuckyNumber.map { "\($0) is a lucky number" }
+
+for luckyNumber in formatedAscendingOddLuckyNumber {
+    print(luckyNumber)
+}
+
+"""
 3. How to accept functions as parameters
 """
 
