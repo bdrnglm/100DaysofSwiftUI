d6255d0aa674f25aa025051ffcb25e68b26b295a
Badran <bdrnglm@gmail.com>
2022-06-10 00:04:27 +0200

    DAY009: Checkpoint 5: 🔗

--- DAY009/DAY009.playground/Contents.swift
+++ DAY009/DAY009.playground/Contents.swift
@@ -9,15 +9,9 @@ Your job is to:
 4. Print the resulting array, one item per line
 """
 
-let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49].filter { !$0.isMultiple(of: 2) }.sorted { $0 < $1 }.map { "\($0) is a lucky number" }
 
-let oddLuckyNumbers = luckyNumbers.filter { $0 % 2 != 0 }
-
-let ascendingOddLuckyNumber = oddLuckyNumbers.sorted { $0 < $1 }
-
-let formatedAscendingOddLuckyNumber = ascendingOddLuckyNumber.map { "\($0) is a lucky number" }
-
-for luckyNumber in formatedAscendingOddLuckyNumber {
+for luckyNumber in luckyNumbers {
     print(luckyNumber)
 }
 
