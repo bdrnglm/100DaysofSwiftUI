a4a12ac7f44d4770f17bffbcc3ed519a75a10bad
Badran <bdrnglm@gmail.com>
2022-06-10 23:35:05 +0200

    How to compute property values dynamically

--- DAY010/DAY010.playground/Contents.swift
+++ DAY010/DAY010.playground/Contents.swift
@@ -1,4 +1,29 @@
 """
+2. How to compute property values dynamically
+"""
+
+struct Employee {
+    let name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+archer.vacationTaken += 4
+archer.vacationRemaining = 5
+print(archer.vacationAllocated)
+
+"""
 1. How to create your own structs
 """
 
@@ -21,28 +46,28 @@ print(wings.artist)
 red.printSummary()
 wings.printSummary()
 
-struct Employee {
-    let name: String
-    var vacationRemaining = 14
+//struct Employee {
+//    let name: String
+//    var vacationRemaining = 14
+//
+//    mutating func takeVacation(days: Int) {
+//        if vacationRemaining > days {
+//            vacationRemaining -= days
+//            print("I'm going on vacation!")
+//            print("Days remaining: \(vacationRemaining)")
+//        } else {
+//            print("Oops! There aren't enough days remaining.")
+//        }
+//    }
+//}
 
-    mutating func takeVacation(days: Int) {
-        if vacationRemaining > days {
-            vacationRemaining -= days
-            print("I'm going on vacation!")
-            print("Days remaining: \(vacationRemaining)")
-        } else {
-            print("Oops! There aren't enough days remaining.")
-        }
-    }
-}
 
-
-var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
-archer.takeVacation(days: 5)
-print(archer.vacationRemaining)
-
-var archer1 = Employee(name: "Sterling Archer", vacationRemaining: 14)
-var archer2 = Employee.init(name: "Sterling Archer", vacationRemaining: 14)
-
-let kane = Employee(name: "Lana Kane")
-let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
+//var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+//archer.takeVacation(days: 5)
+//print(archer.vacationRemaining)
+//
+//var archer1 = Employee(name: "Sterling Archer", vacationRemaining: 14)
+//var archer2 = Employee.init(name: "Sterling Archer", vacationRemaining: 14)
+//
+//let kane = Employee(name: "Lana Kane")
+//let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
