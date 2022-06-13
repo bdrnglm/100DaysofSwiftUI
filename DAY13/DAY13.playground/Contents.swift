35de9c0b9d8d2dba7c07073fb5f1dd4050a4f7c0
Badran <bdrnglm@gmail.com>
2022-06-13 12:52:15 +0200

    DAY13: Checkpoint 8

--- DAY13/DAY13.playground/Contents.swift
+++ DAY13/DAY13.playground/Contents.swift
@@ -1,4 +1,44 @@
 """
+Checkpoint 8
+
+Your challenge is this: make a protocol that describes a building, adding various properties and methods, then create two structs, House and Office, that conform to it. Your protocol should require the following:
+
+A property storing how many rooms it has.
+A property storing the cost as an integer (e.g. 500,000 for a building costing $500,000.)
+A property storing the name of the estate agent responsible for selling the building.
+A method for printing the sales summary of the building, describing what it is along with its other properties.
+"""
+
+protocol Building {
+    var rooms: Int { get set }
+    var cost: Int { get set }
+    var name: String { get set }
+    func summary()
+}
+
+struct House: Building {
+    var rooms = 4
+    var cost = 300_000
+    var name = "Orpi"
+    
+    func summary() {
+        print("This \(rooms) room\(rooms > 1 ? "s" : "") house listed by \(name) is evaluated at \(cost)€.")
+    }
+}
+
+struct Office: Building {
+    var rooms = 8
+    var cost = 500_000
+    var name = "Orpi Pro"
+
+    func summary() {
+        print("This \(rooms) room\(rooms > 1 ? "s" : "") office listed by \(name) is evaluated at \(cost)€.")
+    }
+}
+
+Office().summary()
+
+"""
 3. How to create and use extensions
 """
 
@@ -46,7 +86,7 @@ extension Book {
 }
 
 let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
-s
+
 """
 2. How to use opaque return types
 """
