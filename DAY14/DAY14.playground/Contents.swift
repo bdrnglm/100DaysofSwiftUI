18989cdbf4c0592f23bda37a61784449af19b7eb
Badran <bdrnglm@gmail.com>
2022-06-15 00:28:22 +0200

    DAY14: How to unwrap optionals with nil coalescing

--- DAY14/DAY14.playground/Contents.swift
+++ DAY14/DAY14.playground/Contents.swift
@@ -1,4 +1,34 @@
 """
+3. How to unwrap optionals with nil coalescing
+"""
+
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+//let new = captains["Serenity"] ?? "N/A"
+
+let new = captains["Serenity", default: "N/A"]
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let number = Int(input) ?? 0
+print(number)
+
+"""
 2. How to unwrap optionals with guard
 """
 
