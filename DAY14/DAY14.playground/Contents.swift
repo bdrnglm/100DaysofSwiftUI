c2c974d6e2823392dd5e9f18648ecd42f548fc0e
Badran <bdrnglm@gmail.com>
2022-06-15 00:54:20 +0200

    DAY14: How to handle function failure with optionals

--- DAY14/DAY14.playground/Contents.swift
+++ DAY14/DAY14.playground/Contents.swift
@@ -1,4 +1,23 @@
 """
+5. How to handle function failure with optionals
+"""
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
+
+"""
 4. How to handle multiple optionals using optional chaining
 """
 
