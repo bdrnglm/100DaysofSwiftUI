6ba2719ac1a3025d623d165ac3985d58ab84ba23
Badran <bdrnglm@gmail.com>
2022-06-21 00:10:28 +0200

    DAY20: GuessTheFlag > Color and frames

--- DAY20/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ DAY20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,70 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        VStack {
-            HStack {
-                Text("1")
-                Text("2")
-                Text("3")
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
             }
-            
-            HStack {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            
-            HStack {
-                Text("7")
-                Text("8")
-                Text("9")
-            }
+
+            Text("Your content")
+                .foregroundColor(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
         }
-        
-//        ZStack {
-//            Text("Hello, world!")
-//            Text("This is inside a stack")
-//        }
-        
-//        VStack {
-//            Spacer()
-//            Text("First")
-//            Text("Second")
-//            Text("Third")
-//            Spacer()
-//            Spacer()
-//        }
-        
-//        VStack {
-//            Text("First")
-//            Text("Second")
-//            Text("Third")
-//            Spacer()
-//        }
-        
-//        HStack(spacing: 20) {
-//            Text("Hello, world!")
-//            Text("This is inside a stack")
-//        }
-
-//        VStack(alignment: .leading) {
-//            Text("Hello, world!")
-//            Text("This is inside a stack")
-//        }
-        
-//        VStack(spacing: 20) {
-//            Text("Hello, world!")
-//            Text("This is inside a stack")
-//        }
-
-//        VStack {
-//            Text("Hello, world!")
-//            Text("This is inside a stack")
-//        }
-        
-//        Text("Hello, world!")
-//        Text("This is another text view")
+        .ignoresSafeArea()
     }
 }
 
