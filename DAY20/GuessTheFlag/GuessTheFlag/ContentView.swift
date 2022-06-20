af8f08b4424be7552f2c245a5d6212d58a64547a
Badran <bdrnglm@gmail.com>
2022-06-21 00:34:55 +0200

    DAY20: GuessTheFlag > Showing alert messages

--- DAY20/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ DAY20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,52 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+
     var body: some View {
-
-        Button {
-            print("Edit button was tapped")
-        } label: {
-            Label("Edit", systemImage: "pencil")
-//            Image(systemName: "pencil")
+                
+        Button("Show Alert") {
+            showingAlert = true
         }
-        
-//        Button {
-//            print("Button was tapped")
-//        } label: {
-//            Text("Tap me!")
-//                .padding()
-//                .foregroundColor(.white)
-//                .background(.red)
-//        }
-        
-//        VStack {
-//            Button("Button 1") { }
-//                .buttonStyle(.bordered)
-//            Button("Button 2", role: .destructive) { }
-//                .buttonStyle(.bordered)
-//            Button("Button 3") { }
-//                .buttonStyle(.borderedProminent)
-//                .tint(.mint)
-////            Button("Button 3") { }
-////                .buttonStyle(.borderedProminent)
-//            Button("Button 4", role: .destructive) { }
-//                .buttonStyle(.borderedProminent)
-//        }
-        
-//        Button("Delete selection", role: .destructive, action: executeDelete)
-        
-//        Button("Delete selection", action: executeDelete)
-
-//        Button("Delete selection") {
-//            print("Now deleting…")
-//        }
-        
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("OK", role: .cancel) { }
+//            Button("Delete", role: .destructive) { }
+//            Button("Cancel", role: .cancel) { }
+//            Button("OK") { }
+            
+        } message: {
+            Text("Please read this.")
+        }
     }
-
-    func executeDelete() {
-        print("Now deleting…")
-    }
-
 }
 
 struct ContentView_Previews: PreviewProvider {
