f9feaa33e2bd0879e2c23420760fcd2d1f485327
Badran <bdrnglm@gmail.com>
2022-06-20 23:58:13 +0200

    DAY20: GuessTheFlag > Using stacks to arrange views

---
+++ DAY20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Badran on 20/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            HStack {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            
+            HStack {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            
+            HStack {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
+        
+//        ZStack {
+//            Text("Hello, world!")
+//            Text("This is inside a stack")
+//        }
+        
+//        VStack {
+//            Spacer()
+//            Text("First")
+//            Text("Second")
+//            Text("Third")
+//            Spacer()
+//            Spacer()
+//        }
+        
+//        VStack {
+//            Text("First")
+//            Text("Second")
+//            Text("Third")
+//            Spacer()
+//        }
+        
+//        HStack(spacing: 20) {
+//            Text("Hello, world!")
+//            Text("This is inside a stack")
+//        }
+
+//        VStack(alignment: .leading) {
+//            Text("Hello, world!")
+//            Text("This is inside a stack")
+//        }
+        
+//        VStack(spacing: 20) {
+//            Text("Hello, world!")
+//            Text("This is inside a stack")
+//        }
+
+//        VStack {
+//            Text("Hello, world!")
+//            Text("This is inside a stack")
+//        }
+        
+//        Text("Hello, world!")
+//        Text("This is another text view")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
