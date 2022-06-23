931cf314c69109efb4d7ff70f6298a53a2af98ee
Badran <bdrnglm@gmail.com>
2022-06-23 11:08:23 +0100

    DAY23: ViewsAndModifiers > What is behind the main SwiftUI view?

---
+++ DAY23/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Badran on 23/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
