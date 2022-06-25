27dbafdbd9078ad96c4add5fe21590750a0c50cf
Badran <bdrnglm@gmail.com>
2022-06-25 09:27:41 +0100

    DAY25: BetterRest > Entering numbers with Stepper

---
+++ DAY25/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Badran on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DAY25/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Badran on 25/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
+    var body: some View {
+        Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
