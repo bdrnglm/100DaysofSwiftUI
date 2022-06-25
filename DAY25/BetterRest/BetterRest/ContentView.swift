f300a738d2cfe7355c162659011232060c8b4bac
Badran <bdrnglm@gmail.com>
2022-06-25 10:44:13 +0100

    DAY25: BetterRest > Training a model with Create ML

--- DAY25/BetterRest/BetterRest/ContentView.swift
+++ DAY25/BetterRest/BetterRest/ContentView.swift
@@ -10,27 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        Text(Date.now.formatted(date: .long, time: .omitted))
-//        Text(Date.now.formatted(date: .long, time: .shortened))
-//        Text(Date.now, format: .dateTime.hour().minute().second().day().month().year())
-//        Text(Date.now, format: .dateTime.hour().minute())
-    }
-
-    
-    
-    func trivialExemple() {
-        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
-        
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? Date.now
-
-//        let now = Date.now
-//        let tomorrow = Date.now.addingTimeInterval(86400)
-//        let range = now...tomorrow
+        Text("Hello, world!")
     }
 }
 
