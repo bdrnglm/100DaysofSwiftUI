67a553ca6b0cf6b55ea858da2d513fab8cedaa98
Badran <bdrnglm@gmail.com>
2022-06-25 14:42:31 +0100

    DAY25: Milestone: Project 1-3 > Challenge > Rock, Paper, Scissors

--- DAY25/RockPaperScissors/RockPaperScissors/ContentView.swift
+++ DAY25/RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -9,20 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     let moves = ["✊", "✋", "✌️"]
-    let wMoves = ["✋", "✌️", "✊"]
-    let lMoves = ["✌️", "✊", "✋"]
-
+    
+    @State private var showingScore = false
+    @State private var showingReset = false
+    
+    @State private var scoreTitle = ""
+    
     @State private var randomMove = Int.random(in: 0 ..< 3)
     @State private var shouldWin = Bool.random()
     @State private var move = "✊"
     
     @State private var score = 0
+    @State private var round = 1
     
+    @State private var correctMove = ""
+
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.cyan, .purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
+
             VStack {
+                Text("Round: \(round)")
+                    .font(.largeTitle)
+
+                Spacer()
+
                 Text(shouldWin ? "WIN" : "LOSE")
                     .padding()
                     .font(.largeTitle)
@@ -42,55 +54,73 @@ struct ContentView: View {
                         Button(move) {
                             check(playerMove: move)
                         }
-                        .font(.system(size: 50))
+                        .font(.system(size: 65))
                         .padding()
                     }
                 }
                 
                 Spacer()
+                
+                Text("Score: \(score)")
+                    .font(.largeTitle)
+                
+                Spacer()
             }
             .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: prepareGame)
+        } message: {
+            if scoreTitle == "Correct" {
+                Text("You chose the right move!")
+            } else {
+                Text("\"\(correctMove)\" was the correct move to play")
+            }
+        }
+        .alert("Game Over", isPresented: $showingReset) {
+            Button("Restart", action: resetGame)
+        } message: {
+            Text("Your final score is \(score)")
         }
     }
     
     func check(playerMove: String) {
-//        let CPUMove = moves[randomMove]
-
+        
         if shouldWin {
-            if playerMove == wMoves[randomMove] {
-                print("👍")
-            }
+            correctMove = moves[(randomMove + 1) % moves.count]
         } else {
-            if playerMove == lMoves[randomMove] {
-                print("👍")
-            }
+            correctMove = moves[(randomMove + (moves.count - 1)) % moves.count]
         }
-//        if CPUMove == playerMove {
-//            print("DRAW")
-//        } else if CPUMove == "✊" {
-//            if playerMove == "✋" {
-//                print("WON")
-//            } else {
-//                print("LOST")
-//            }
-//        } else if CPUMove == "✋" {
-//            if playerMove == "✌️" {
-//                print("WON")
-//            } else {
-//                print("LOST")
-//            }
-//        } else if CPUMove == "✌️" {
-//            if playerMove == "✊" {
-//                print("WON")
-//            } else {
-//                print("LOST")
-//            }
-//        }
+
+        if playerMove == correctMove {
+            score += 1
+            scoreTitle = "Correct"
+        } else {
+            score = score > 0 ? score - 1 : 0
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+        round += 1
+    }
+    
+    func prepareGame() {
+        if round >= 10 {
+            showingReset = true
+        }
+        randomMove = Int.random(in: 0 ..< 3)
+        shouldWin = Bool.random()
+    }
+    
+    func resetGame() {
+        score = 0
+        round = 0
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
