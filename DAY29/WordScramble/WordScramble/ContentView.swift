81dad10bd416abb201882cb50c871b378ca5edee
Badran <bdrnglm@gmail.com>
2022-06-29 11:35:12 +0100

    DAY29: WordScramble > Introducing List, your best friend

---
+++ DAY29/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Badran on 29/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
+    var body: some View {
+        List {
+            Text("Static Row")
+
+            ForEach(people, id: \.self) {
+                Text($0)
+            }
+            
+            Text("Static Row")
+        }
+        
+//        List(people, id: \.self) {
+//            Text($0)
+//        }
+
+//        List(0 ..< 5) {
+//            Text("Dynamic row \($0)")
+//            Section("Section 1") {
+//                Text("Static Row 1")
+//                Text("Static Row 2")
+//            }
+//
+//            Section("Section 2") {
+//                ForEach(0 ..< 5) {
+//                    Text("Dynamic Row \($0)")
+//                }
+//            }
+//
+//            Section("Section 3"){
+//                Text("Static Row 3")
+//                Text("Static Row 4")
+//            }
+//        }
+//        .listStyle(.grouped)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
