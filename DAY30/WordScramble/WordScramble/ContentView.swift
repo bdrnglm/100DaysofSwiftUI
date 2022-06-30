20878eba9f70772d67ba7639fcc56fdae1e73c43
Badran <bdrnglm@gmail.com>
2022-06-30 08:13:44 +0100

    DAY30: WordScramble > Adding to a list of words

---
+++ DAY30/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Badran on 29/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                }
+                .onSubmit(addNewWord)
+                .textInputAutocapitalization(.never)
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle(rootWord)
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
