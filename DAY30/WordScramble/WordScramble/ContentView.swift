1d2628f07ad58d1dcc54d330129412e8a7b75d4d
Badran <bdrnglm@gmail.com>
2022-06-30 08:27:10 +0100

    DAY30: WordScramble > Running code when our app launches

--- DAY30/WordScramble/WordScramble/ContentView.swift
+++ DAY30/WordScramble/WordScramble/ContentView.swift
@@ -30,8 +30,9 @@ struct ContentView: View {
                     }
                 }
             }
+            .navigationTitle(rootWord)
+            .onAppear(perform: startGame)
         }
-        .navigationTitle(rootWord)
     }
     
     func addNewWord() {
@@ -42,6 +43,19 @@ struct ContentView: View {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle")
     }
 }
 
