818057f5d7ab0e9dba57e9a0af26dd8bec139e1c
Badran <bdrnglm@gmail.com>
2022-07-02 13:32:27 +0100

    DAY32: Animation > Customizing animations in SwiftUI

--- DAY32/Animations/Animations/ContentView.swift
+++ DAY32/Animations/Animations/ContentView.swift
@@ -12,15 +12,35 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap Me") {
-            animationAmount += 1
+//            animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeIn(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount)
+        )
+        .onAppear {
+            animationAmount = 2
+        }
+
+//        .animation(
+//            .easeIn(duration: 1)
+////            .easeIn(duration: 2)
+//                .repeatForever(autoreverses: true),
+////                .repeatCount(3, autoreverses: true),
+////                .delay(1),
+//            value: animationAmount)
+//        .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
+//        .animation(.easeOut, value: animationAmount)
     }
 }
 
