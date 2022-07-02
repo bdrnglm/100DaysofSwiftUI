6b47accb7b607b64938f4b540da812f959c76c09
Badran <bdrnglm@gmail.com>
2022-07-02 13:41:58 +0100

    DAY32: Animation > Animating bindings

--- DAY32/Animations/Animations/ContentView.swift
+++ DAY32/Animations/Animations/ContentView.swift
@@ -11,36 +11,25 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-//            animationAmount += 1
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scaler amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in:1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeIn(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount)
-        )
-        .onAppear {
-            animationAmount = 2
-        }
-
-//        .animation(
-//            .easeIn(duration: 1)
-////            .easeIn(duration: 2)
-//                .repeatForever(autoreverses: true),
-////                .repeatCount(3, autoreverses: true),
-////                .delay(1),
-//            value: animationAmount)
-//        .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
-//        .animation(.easeOut, value: animationAmount)
     }
 }
 
