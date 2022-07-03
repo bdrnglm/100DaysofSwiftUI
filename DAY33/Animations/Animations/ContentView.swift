be3821111cebaba9973faf83789a42aa0c8c9f85
Badran <bdrnglm@gmail.com>
2022-07-03 15:40:02 +0100

    DAY33: Animation > Showing and hiding views with transitions

--- DAY33/Animations/Animations/ContentView.swift
+++ DAY33/Animations/Animations/ContentView.swift
@@ -8,49 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
-
+    @State private var isShowingRed = false
+    
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .cyan : .purple)
-                    .offset(dragAmount)
-                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//                    .transition(.scale)
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
     }
-
-//    @State private var dragAmount = CGSize.zero
-//
-//    var body: some View {
-//        LinearGradient(gradient: Gradient(colors: [.cyan, .purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
-//            .frame(width: 300, height: 200)
-//            .clipShape(RoundedRectangle(cornerRadius: 10))
-//            .offset(dragAmount)
-//            .gesture(
-//                DragGesture()
-//                    .onChanged { dragAmount = $0.translation }
-//                    .onEnded { _ in
-//                        withAnimation(.spring()) {
-//                            dragAmount = .zero
-//                        }
-//                    }
-//            )
-//            .animation(.spring(), value: dragAmount)
-//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
