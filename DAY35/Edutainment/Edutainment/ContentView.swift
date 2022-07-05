caef93cbf222aa3491aeb85ca26c3efc2c0b3c26
Badran <bdrnglm@gmail.com>
2022-07-06 01:13:04 +0200

    DAY35: Milestone: Project 3-6 > Challenge > Edutainment > Generate list of TextField according to amount of questions requested

--- DAY35/Edutainment/Edutainment/ContentView.swift
+++ DAY35/Edutainment/Edutainment/ContentView.swift
@@ -11,8 +11,14 @@ struct ContentView: View {
     @State private var table = 2
     
     let questionAmounts = [5, 10, 20]
-    @State private var questionAmount = 0
+    @State private var questionAmount = 5
+    
+    @State private var answer = ""
+    
+    @FocusState private var answerIsFocused: Bool
 
+    @State private var questionTable = Array(0...20)
+    
     var body: some View {
         NavigationView {
             Form {
@@ -23,6 +29,7 @@ struct ContentView: View {
                         }
                     }
                     .pickerStyle(.segmented)
+                    .disabled(true)
                 } header: {
                     Text("Multiplication tables to practice")
                 }
@@ -37,8 +44,29 @@ struct ContentView: View {
                 } header: {
                     Text("Number of questions to be asked")
                 }
+                
+                Section {
+                    List(0 ..< questionAmount, id: \.self) { ligne in
+                        HStack {
+                            Text("\(table) x \(questionTable[ligne]) =")
+                            TextField("?", text: $answer)
+                                .keyboardType(.decimalPad)
+                                .focused($answerIsFocused)
+                        }
+                    }
+                }
             }
             .navigationTitle("Edutainment")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        answerIsFocused = false
+                        questionTable.shuffle()
+                    }
+                }
+            }
         }
     }
 }
