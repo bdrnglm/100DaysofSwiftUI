3fb9464e62aeaab880c18d6d579541af6399fe05
Badran <bdrnglm@gmail.com>
2022-07-06 12:58:51 +0200

    DAY36: iExpense > Showing and hiding views

--- DAY36/iExpense/iExpense/ContentView.swift
+++ DAY36/iExpense/iExpense/ContentView.swift
@@ -7,20 +7,31 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    let name: String
+    
+    var body: some View {
+        VStack{
+            Text("Hello, \(name)")
+            Button("Dismiss") {
+                dismiss()
+            }
+            .padding()
+        }
+    }
 }
 
 struct ContentView: View {
-    @StateObject private var user = User()
+    @State private var showingSheet = false
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        Button("Show sheet") {
+            showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "@bdrnglm")
         }
     }
 }
