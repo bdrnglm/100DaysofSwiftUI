b64dde4dd15fe691b31dc3c61b3207969ef36133
Badran <bdrnglm@gmail.com>
2022-07-06 13:10:22 +0200

    DAY36: iExpense > Deleting items using onDelete()

--- DAY36/iExpense/iExpense/ContentView.swift
+++ DAY36/iExpense/iExpense/ContentView.swift
@@ -7,32 +7,34 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        VStack{
-            Text("Hello, \(name)")
-            Button("Dismiss") {
-                dismiss()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
             }
-            .padding()
+            .navigationTitle("onDelete()")
+            .toolbar {
+                EditButton()
+            }
         }
     }
-}
-
-struct ContentView: View {
-    @State private var showingSheet = false
     
-    var body: some View {
-        Button("Show sheet") {
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "@bdrnglm")
-        }
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
