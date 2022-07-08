b8af2c4d09295d756c6e0d4d9130a6a9fd265c53
Badran <bdrnglm@gmail.com>
2022-07-08 07:23:50 +0200

    DAY38: iExpense > initial commit

---
+++ DAY38/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Badran on 06/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: "TND"))
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpenses")
+            .toolbar {
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
