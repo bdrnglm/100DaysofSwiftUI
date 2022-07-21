6957b3c39769e4fb3d12a626eb0a4a72e67c16d4
Badran <bdrnglm@gmail.com>
2022-07-21 18:27:40 +0200

    DAY45: Drawing > Special effects in SwiftUI: blurs, blending, and more

--- DAY45/Drawing/Drawing/ContentView.swift
+++ DAY45/Drawing/Drawing/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = 0.0
     var body: some View {
-        ZStack {
+        VStack {
             Image("Patrick")
-                .colorMultiply(.red)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .saturation(amount)
+                .blur(radius: (1 - amount) * 20)
 
-//            Rectangle()
-//                .fill(.red)
-//                .blendMode(.multiply)
+//            ZStack {
+//                Circle()
+//                    .fill(Color(red: 1, green: 0, blue: 0))
+////                    .fill(.red)
+//                    .frame(width: 200 * amount)
+//                    .offset(x: -50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+//                    .fill(Color(red: 0, green: 1, blue: 0))
+////                    .fill(.green)
+//                    .frame(width: 200 * amount)
+//                    .offset(x: 50, y: -80)
+//                    .blendMode(.screen)
+//
+//                Circle()
+//                    .fill(Color(red: 0, green: 0, blue: 1))
+////                    .fill(.blue)
+//                    .frame(width: 200 * amount)
+//                    .blendMode(.screen)
+//            }
+//            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
         }
-        .frame(width: 400, height: 500)
-        .clipped()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
+        
+//        ZStack {
+//            Image("Patrick")
+////                .colorMultiply(.red)
+//
+////            Rectangle()
+////                .fill(.red)
+////                .blendMode(.multiply)
+//        }
+//        .frame(width: 400, height: 500)
+//        .clipped()
     }
 }
 
