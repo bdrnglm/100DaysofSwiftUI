e84be78fbe01b1238410fe58ef5632a243a5ea13
Badran <bdrnglm@gmail.com>
2022-07-28 15:24:36 +0100

    DAY46: Drawing > Wrap up > Make a ColorCyclingRectangle

--- DAY46/Drawing/Drawing/ContentView.swift
+++ DAY46/Drawing/Drawing/ContentView.swift
@@ -7,6 +7,42 @@
 
 import SwiftUI
 
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0 ..< steps) { value in
+                Rectangle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct Arrow: Shape {
     var insetAmount = 0.0
 
@@ -28,20 +64,27 @@ struct Arrow: Shape {
 }
 
 struct ContentView: View {
-    @State private var lineWidth = 1.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
         VStack {
-            Arrow()
-                .stroke(.red, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-                .padding(.top)
+            ColorCyclingRectangle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+                
+            Slider(value: $colorCycle)
         }
-        .contentShape(Rectangle())
-        .onTapGesture {
-            withAnimation {
-                lineWidth = Double.random(in: 1...50)
-            }
-        }
+        
+//        VStack {
+//            Arrow()
+//                .stroke(.red, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+//                .padding(.top)
+//        }
+//        .contentShape(Rectangle())
+//        .onTapGesture {
+//            withAnimation {
+//                lineWidth = Double.random(in: 1...50)
+//            }
+//        }
     }
 }
 
