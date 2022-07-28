7f12fa36ddc780e01e02b330634f43db41cc9c1e
Badran <bdrnglm@gmail.com>
2022-07-28 13:45:49 +0200

    DAY46: Drawing > Wrap up > Create Arrow Shape

--- DAY46/Drawing/Drawing/ContentView.swift
+++ DAY46/Drawing/Drawing/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
+struct Arrow: Shape {
+    let size: CGSize
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: size.width / 2, y: 0))
+        path.addLine(to: CGPoint(x: size.width / 4, y: size.height / 4))
+        path.addLine(to: CGPoint(x: size.width - size.width / 4, y: size.height / 4))
+        path.addLine(to: CGPoint(x: size.width / 2, y: 0))
+        
+        path.move(to: CGPoint(x: size.width / 2 - size.width / 10, y: size.height / 4))
+        path.addLine(to: CGPoint(x: size.width / 2 - size.width / 10, y: (size.height / 4) * 2))
+        path.addLine(to: CGPoint(x: size.width / 2 + size.width / 10, y: (size.height / 4) * 2))
+        path.addLine(to: CGPoint(x: size.width / 2 + size.width / 10, y: size.height / 4))
+
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        GeometryReader { geometry in
+            VStack {
+                Arrow(size: geometry.size)
+                    .padding(.top)
+            }
+        }
     }
 }
 
