e3ce2bffb3ffd1a84548c48377e84eb0de0d9cdf
Badran <bdrnglm@gmail.com>
2022-07-29 10:44:44 +0100

    DAY47: Milestone: Projects 7-9 > Challenge > HabitTracking > initial commit

---
+++ DAY47/HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Badran on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
