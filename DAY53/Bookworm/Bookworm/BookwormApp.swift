cb9bc0102ed5565bfa479240b39e47bb24fd3a52
Badran <bdrnglm@gmail.com>
2022-08-04 20:01:33 +0200

    DAY53: Bookworm > initial commit

---
+++ DAY53/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Badran on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
