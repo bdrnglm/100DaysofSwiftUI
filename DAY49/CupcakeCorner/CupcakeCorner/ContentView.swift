69284e96816cad3d1788ba45623847a0f2926aec
Badran <bdrnglm@gmail.com>
2022-08-04 14:09:22 +0200

    DAY49: CupcakeCorner > Loading an image from a remote server

--- DAY49/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ DAY49/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,47 +7,31 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
             }
         }
-        .task {
-            await loadData()
-        }
-    }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
+        .frame(width: 200, height: 200)
         
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+//            image
+//                .resizable()
+//                .scaledToFit()
+//        } placeholder: {
+//            Color.red
+//        }
+//        .frame(width: 200, height: 200)
 
-            if let decodeResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodeResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
     }
 }
 
