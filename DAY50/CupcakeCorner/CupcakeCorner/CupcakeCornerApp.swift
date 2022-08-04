404e8056dcf4f3d790fa93c2b420c9ed4a3b5652
Badran <bdrnglm@gmail.com>
2022-08-04 14:57:29 +0200

    DAY50: CupcakeCorner > initial commit

---
+++ DAY50/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Badran on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
