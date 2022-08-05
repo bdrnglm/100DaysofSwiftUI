1fefecc68fcb236e6fcfd1d9d71b255db68b7d83
Badran <bdrnglm@gmail.com>
2022-08-05 18:11:58 +0200

    DAY55: Bookworm > Deleting from a Core Data fetch request

--- DAY55/Bookworm/Bookworm/ContentView.swift
+++ DAY55/Bookworm/Bookworm/ContentView.swift
@@ -12,9 +12,6 @@ struct ContentView: View {
     @FetchRequest(sortDescriptors: [
         SortDescriptor(\.title),
         SortDescriptor(\.author)
-        
-//        SortDescriptor(\.title, order: .reverse)
-//        SortDescriptor(\.title)
     ]) var books: FetchedResults<Book>
     
     @State private var showingAddScreen = false
@@ -40,9 +37,14 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showingAddScreen.toggle()
@@ -55,6 +57,16 @@ struct ContentView: View {
                 AddBookView()
             }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            
+            moc.delete(book)
+        }
+        
+        try? moc.save()
     }
 }
 
