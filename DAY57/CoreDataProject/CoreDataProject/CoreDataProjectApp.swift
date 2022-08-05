5a484c481fd43b5b940e6f4d1b6396dc975e0204
Badran <bdrnglm@gmail.com>
2022-08-05 19:49:31 +0200

    DAY57: CoreDataProject > initial commit

---
+++ DAY57/CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Badran on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
