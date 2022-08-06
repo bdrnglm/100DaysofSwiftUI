9212511bc93df1255efb1ff96439cb579f981713
Badran <bdrnglm@gmail.com>
2022-08-06 19:09:46 +0200

    DAY59: CoreDataProject > Wrap up > Challenge > Accept a string parameter as predicate

--- DAY59/CoreDataProject/CoreDataProject/FilteredList.swift
+++ DAY59/CoreDataProject/CoreDataProject/FilteredList.swift
@@ -19,8 +19,8 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
         }
     }
     
-    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
-        _fetchRequests = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+    init(filterKey: String, filterValue: String, predicate: String, @ViewBuilder content: @escaping (T) -> Content) {
+        _fetchRequests = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K \(predicate) %@", filterKey, filterValue))
         self.content = content
     }
 }
