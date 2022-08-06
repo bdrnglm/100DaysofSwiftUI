690f5b9d9fcceb68cd07082aec262b1d83fb4105
Badran <bdrnglm@gmail.com>
2022-08-06 13:48:15 +0200

    DAY57: CoreDataProject > Creating NSManagedObject subclasses

---
+++ DAY57/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Badran on 06/08/2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
