0bdf139c5994c86d61ecda09f19e23ac710aaa3f
Badran <bdrnglm@gmail.com>
2022-08-13 11:37:02 +0200

    DAY63: Instafilter > Integrating Core Image with SwiftUI

--- DAY63/Instafilter/Instafilter/ContentView.swift
+++ DAY63/Instafilter/Instafilter/ContentView.swift
@@ -6,10 +6,57 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        
+//        let currentFilter = CIFilter.crystallize()
+//        let currentFilter = CIFilter.pixellate()
+//        let currentFilter = CIFilter.sepiaTone()
+        
+        currentFilter.inputImage = beginImage
+        
+        let amount = 1.0
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount, forKey: kCIInputScaleKey) }
+
+
+//        currentFilter.radius = 1000
+//        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+        
+//        currentFilter.radius = 100
+//        currentFilter.scale = 100
+//        currentFilter.intensity = 1
+
+        guard let outputImage = currentFilter.outputImage else { return }
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            
+            image = Image(uiImage: uiImage)
+        }
+            
     }
 }
 
