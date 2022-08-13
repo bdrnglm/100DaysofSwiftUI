2509c68368c2a2628798839253255f75114f5714
Badran <bdrnglm@gmail.com>
2022-08-13 12:26:41 +0200

    DAY64: Instafilter > initial commit

---
+++ DAY64/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Badran on 12/08/2022.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                showingImagePicker = true
+            }
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
