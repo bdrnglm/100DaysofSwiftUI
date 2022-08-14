d9f007d80c2246e4f43f381d91b317c91e4d8169
Badran <bdrnglm@gmail.com>
2022-08-14 16:57:43 +0200

    DAY65: Instafilter > initial commit

---
+++ DAY65/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Badran on 12/08/2022.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
