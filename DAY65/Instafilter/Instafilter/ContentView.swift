46d693f146e6e2ca983464353b2a026b59efc61a
Badran <bdrnglm@gmail.com>
2022-08-14 17:08:31 +0200

    DAY65: Instafilter > Building our basic UI

--- DAY65/Instafilter/Instafilter/ContentView.swift
+++ DAY65/Instafilter/Instafilter/ContentView.swift
@@ -10,9 +10,53 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+                    
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    // select an image
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+                    
+                    Spacer()
+
+                    Button("Save", action: save)
+//                    Button("Save") {
+//                        // save the picture
+//                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
     }
+    
+    func save() { }
 }
 
 struct ContentView_Previews: PreviewProvider {
