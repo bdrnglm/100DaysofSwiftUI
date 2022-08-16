31884cd8a669576488a2d1986cd73db8c9c1349b
Badran <bdrnglm@gmail.com>
2022-08-16 16:59:40 +0200

    DAY73: Bucketlist > Wrap Up: Challenge > initial commit

---
+++ DAY73/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Badran on 14/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
