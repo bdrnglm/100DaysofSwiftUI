2e83d602d538fe8cd8cf905c303ba8b196cb2e86
Badran <bdrnglm@gmail.com>
2022-08-16 12:19:35 +0200

    DAY71: Bucketlist > Downloading data from Wikipedia

---
+++ DAY71/BucketList/BucketList/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  BucketList
+//
+//  Created by Badran on 16/08/2022.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
