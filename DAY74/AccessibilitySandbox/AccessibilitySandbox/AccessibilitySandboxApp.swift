6be09b658f351f6356a66f9632422d349940614d
Badran <bdrnglm@gmail.com>
2022-08-17 16:06:18 +0200

    DAY74: AccessibilitySandbox > initial commit

---
+++ DAY74/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Badran on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
