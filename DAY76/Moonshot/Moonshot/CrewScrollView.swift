bf63575304abc1e9074539c81f5b0542715ba167
Badran <bdrnglm@gmail.com>
2022-08-17 19:08:02 +0200

    DAY76: AccessibilitySandbox > Wrap Up: Challenge > Fix Moonshot (correction)

--- DAY76/Moonshot/Moonshot/CrewScrollView.swift
+++ DAY76/Moonshot/Moonshot/CrewScrollView.swift
@@ -31,6 +31,7 @@ struct CrewScrollView: View {
                                 Text(crewMember.astronaut.name)
                                     .foregroundColor(.white)
                                     .font(.headline)
+                                    .accessibilityLabel(crewMember.astronaut.name.replacingOccurrences(of: ".", with: " "))
                                 Text(crewMember.role)
                                     .foregroundColor(.secondary)
                             }
