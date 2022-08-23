d6d78f08333495d6f50aa026eb264fe5100f52df
Badran <bdrnglm@gmail.com>
2022-08-23 12:56:23 +0200

    Revert "DAY77: Milestone: Projects 13-15 > HotProspects > Wrap PHPickerViewController so it can be used to select photos"
    This reverts commit b458e38abe174bd65708d201eb9307e761680c86.

--- DAY77/HotProspects/HotProspects/ContentView.swift
+++ DAY77/HotProspects/HotProspects/ContentView.swift
@@ -8,29 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
-    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
-            }
-        }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in loadImage()}
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        Text("Hello, world!")
+            .padding()
     }
 }
 
